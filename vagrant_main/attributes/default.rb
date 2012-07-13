4cf4e88cabbd1b0c1c3265de01025bb4da0caed4
Paul Schwarz <paulsschwarz@gmail.com>
2012-07-13 19:42:12 +0300

    Removed vagrant_main because it should stay outside of here.

--- vagrant_main/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default[:app][:server_name]        = "localhost"
-default[:app][:server_aliases]     = ["*.localhost"]
-default[:app][:docroot]            = "/home/vagrant/web-app/httpdocs"
-default[:app][:kohana_environment] = "development"
-default[:app][:extra_packages]     = []
