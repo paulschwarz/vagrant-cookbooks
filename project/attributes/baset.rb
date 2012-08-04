c451bd405250adb27a3e06deeeda83ea3af0c0d0
Paul Schwarz <paulsschwarz@gmail.com>
2012-08-04 21:04:22 +0300

    Corrected doc root.

--- project/attributes/baset.rb
+++ project/attributes/baset.rb
@@ -1,11 +1,11 @@
 default[:app][:staging][:server_name]        = "bt.mss.co.ke"
 default[:app][:staging][:server_aliases]     = ["*.bt.mss.co.ke"]
 default[:app][:staging][:docbase]            = "/home/baset/staging"
-default[:app][:staging][:docroot]            = "/home/baset/staging/current"
+default[:app][:staging][:docroot]            = "/home/baset/staging/current/public"
 default[:app][:staging][:server_env]         = "staging"
 
 default[:app][:production][:server_name]     = "basetitanium.com"
 default[:app][:production][:server_aliases]  = ["*.basetitanium.com"]
 default[:app][:production][:docbase]         = "/home/baset/production"
-default[:app][:production][:docroot]         = "/home/baset/production/current"
+default[:app][:production][:docroot]         = "/home/baset/production/current/public"
 default[:app][:production][:server_env]      = "production"
